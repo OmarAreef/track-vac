51f961e84cf6bc34979654e76bccce97e81a044f
amerasalah7 <amera.salah1982000@gmail.com>
2021-09-23 01:27:17 +0200

    amera

--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,17 @@ const seedDB = async () => {
     const sun = new Day({ day: 'sun', From: '8:00', To: '21:00' });
     const mon = new Day({ day: 'mon', From: '8:00', To: '21:00' });
     const tue = new Day({ day: 'tue', From: '8:00', To: '21:00' });
+    const wed = new Day({ day: 'wed', From: '8:00', To: '21:00' });
+    const thu = new Day({ day: 'thu', From: '8:00', To: '21:00' });
+    const fri = new Day({ day: 'fri', From: '8:00', To: '21:00' });
+    const sat = new Day({ day: 'sat', From: '8:00', To: '21:00' });
     await sun.save();
     await mon.save();
     await tue.save();
+    await wed.save();
+    await thu.save();
+    await fri.save();
+    await sat.save();
     for (let i = 0; i < 9; i++) {
         const center = new Center({
             name: helper[i].name,
@@ -39,7 +47,7 @@ const seedDB = async () => {
             governorateArabic: helper[i].governorateArabic,
             districtArabic: helper[i].districtArabic,
             image: helper[i].image,
-            workingHours: [sun, mon, tue]
+            workingHours: [sun, mon, tue, wed, thu, fri, sat]
         })
         await center.save();
     }
