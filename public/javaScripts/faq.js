49db2f52599a058bac982e54725ead2327479af7
amerasalah7 <amera.salah1982000@gmail.com>
2021-10-23 22:04:47 +0200

    Amera

--- public/javaScripts/faq.js
+++ public/javaScripts/faq.js
@@ -1,29 +1,54 @@
 const items = document.querySelectorAll(".accordion a");
- 
-function toggleAccordion(){
+
+function toggleAccordion() {
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
 }
- 
+
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 let dict = {
   'en': {
     'Header': 'Frequently Asked Questions',
     'Q1': 'What are the steps for writting a review?',
-    'Q1Answer': 'You have to make sure first that you are signed in.To sign in, you have to be vaccinated by entering a valid registration number and the last for digits of your national ID.Then, you can simply add your review.',
+    'Q1Answer': 'You have to make sure first that you are signed in. To sign in :- ',
+    'Q1Answer2': 'Enter the 20-digit valid registration number.',
+    'Q1Answer3': 'Enter the last four digits of your national ID.',
+    'Q1Answer4': 'Now you can add your review.',
     'Q2': 'How can I search for a specific center?',
-    'Q2Answer': 'First you have to naviagte to the start tracking You have 2 options: 1-To search for it in the Start Track page. 2-You can filter your target center by governorate then by district then by your center.',
-   'Q3': 'Is there is a way to report a review?',
-    'Q3Answer': 'Yes but you have first to be signed in.If you find an appropriate review you can report it by mark it as Spam, Harassment or inappropriate.'
+    'Q2Answer1': 'Navigate to start tracking.',
+    'Q2Answer2': 'You have two options :-',
+    'Q2Answer3': 'Search for it in the Start Track page.',
+    'Q2Answer4': 'Filter your target center by governorate then by district then by your center.',
+    'Q3': 'Is there is a way to report reviews/questions/answers?',
+    'Q3Answer': 'Yes, but you have first to be signed in.',
+    'Q3Answer2': 'To report a review, question or answer :-',
+    'Q3Answer3': 'Click on the report button beside this review/question/answer.',
+    'Q3Answer4': 'Specify the reason for reporting it :- ',
+    'Q3Answer5': 'Spam.',
+    'Q3Answer6': 'Harassment. ',
+    'Q3Answer7': 'Inappropriate.'
+
   },
   'ar': {
     'Header': 'الأسئلة الشائعة',
-    'Q1': 'ما هي خطوات كتابة الاراء؟',
-    'Q1Answer': 'يجب عليك التأكد أولاً من تسجيل الدخول. لتسجيل الدخول ، يجب أن تحصل على التطعيم عن طريق إدخال رقم تسجيل صالح وآخر رقم من أرقام الهوية الوطنية الخاصة بك.بعد ذلك ، يمكنك ببساطة إضافة رأيك', 
+    'Q1': 'ما هي خطوات كتابة الآراء؟',
+    'Q1Answer': 'يجب أن تتأكد أولاً من أنك قمت بتسجيل الدخول. لتسجيل الدخول: - ',
+    'Q1Answer2': 'أدخل رقم التسجيل الصحيح المكون من 20 رقمًا.',
+    'Q1Answer3': 'أدخل آخر أربعة أرقام من بطاقة هويتك الوطنية.',
+    'Q1Answer4': 'الآن يمكنك إضافة رأيك.',
     'Q2': 'كيف يمكنني البحث عن مركز معين؟',
-    'Q2Answer': 'أولا عليك أن تنتقل إلى تتبع البدء لديك خياران: 1 - للبحث عنه في صفحة بدء المسار.  2-يمكنك تصفية المركز المستهدف حسب المحافظة ثم المنطقة ثم المركز الخاص بك.',
-    'Q3': 'هل هناك طريقة للإبلاغ عن الاراء ؟',
-    'Q3Answer': 'عم ولكن يجب عليك أولا تسجيل الدخول. إذا وجدت تعليقًا مناسبًا ، يمكنك الإبلاغ عنه عن طريق وضع علامة عليه كرسالة غير مرغوب فيها أو مضايقة أو غير ملائمة.'
+    'Q2Answer1': 'انتقل ل بدء التتبع.',
+    'Q2Answer2': 'لديك خياران: -',
+    'Q2Answer3': 'ابحث عنها في صفحة بدء التتبع.',
+    'Q2Answer4': 'قم بتصفية المركز المستهدف حسب المحافظة ثم المنطقة ثم المركز الخاص بك.',
+    'Q3': 'هل هناك طريقة للإبلاغ عن الآراء/الأسئلة/الأجوبة ؟',
+    'Q3Answer': 'نعم ، ولكن يجب عليك أولاً تسجيل الدخول.',
+    'Q3Answer2': 'للإبلاغ عن مراجعة أو سؤال أو إجابة: -',
+    'Q3Answer3': 'انقر على زر الإبلاغ بجانب هذا الاستعراض / السؤال / الإجابة.',
+    'Q3Answer4': 'حدد سبب الإبلاغ عنها: - ',
+    'Q3Answer5': 'رسائل إلكترونية مزعجة.',
+    'Q3Answer6': 'إزعاج. ',
+    'Q3Answer7': 'غير مناسب.'
   }
 }
 const dictLang = dict[language];
@@ -33,11 +58,11 @@ for (let attr in dictLang) {
   element.innerHTML = dictLang[attr]
 }
 if (language === 'ar') {
-  document.querySelector('#lang-ar').setAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" )
-  document.querySelector('#lang-en').removeAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" )
-  let html =  document.querySelector('html')
-  html.setAttribute('lang' , 'ar')
-  html.setAttribute('dir' , 'rtl')
+  document.querySelector('#lang-ar').setAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css")
+  document.querySelector('#lang-en').removeAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css")
+  let html = document.querySelector('html')
+  html.setAttribute('lang', 'ar')
+  html.setAttribute('dir', 'rtl')
 
 }
 else {
@@ -46,13 +71,13 @@ else {
   // })
   // document.querySelector('body').classList.remove('rtl')
   // document.querySelector('#language-css').removeAttribute('href')
-  document.querySelector('#lang-ar').removeAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" )
-  document.querySelector('#lang-en').setAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" )
+  document.querySelector('#lang-ar').removeAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css")
+  document.querySelector('#lang-en').setAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css")
   // document.querySelector('#language-css').setAttribute('href',"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css")
   // document.querySelector('#language-css').setAttribute('href', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" )
-  let html =  document.querySelector('html')
-  html.setAttribute('lang' , 'en' )
-  html.removeAttribute('dir' )
-  
+  let html = document.querySelector('html')
+  html.setAttribute('lang', 'en')
+  html.removeAttribute('dir')
+
 
 }
